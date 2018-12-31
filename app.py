195e49563ea56e592ce023d25753cd07b2eb90a7
Saroj Humagain <sarojhumagain23@gmail.com>
2018-12-31 13:58:58 +0545

    Boilerplate and form validation done

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+# from datetime import datetime
+# from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '18871a99bd42fd0dfb4b7909d9c5c7f309'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key= True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     password = db.Column(db.String(60), nullable=False)
+#
+#     def __repr__(self):
+#         return f"User('{self.username}','{self.email}')"
+
+#app.debug = True
+@app.route('/')
+def home():
+    #if 'username' in session:
+    #    return 'You are logged in as ' + session['username']
+    return render_template("home.html")
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'asd123':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html",title = 'Login', form = form)
+
+@app.route('/register', methods = ['GET','POST'] )
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title = "Register", form = form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
